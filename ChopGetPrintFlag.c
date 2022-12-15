93d0e235ed60da3e2d7068b2caecdc0934dd5685
sclaiborne <scott@loupe.team>
2022-12-14 19:50:03 -0800

    Remove default STRING flag
    It works better without it as we can do string length protection without it

--- ChopGetPrintFlag.c
+++ ChopGetPrintFlag.c
@@ -30,9 +30,9 @@ void ChopGetPrintFlag(char* dest, UDINT type) {
 		case VAR_TYPE_UDINT:
 			strcpy(dest, "%u");
 			break;
-		case VAR_TYPE_STRING:
-			strcpy(dest, "%s");
-			break;
+//		case VAR_TYPE_STRING:
+//			strcpy(dest, "%s");
+//			break;
 		default:
 			strcpy(dest, "");
 			break;
