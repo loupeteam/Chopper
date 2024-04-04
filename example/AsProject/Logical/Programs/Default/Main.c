a3a0dd7cff59e47c1dd87897672952d91f868eaa
sclaiborne <scott@loupe.team>
2024-04-04 14:27:07 -0700

    Add default task for testing

--- example/AsProject/Logical/Programs/Default/Main.c
+++ example/AsProject/Logical/Programs/Default/Main.c
@@ -7,11 +7,41 @@
 
 void _INIT ProgramInit(void)
 {
-
+	templateDynamic.snippet = &snippetDynamic[0];
+	templateDynamic.maxSnippets = sizeof(snippetDynamic)/sizeof(snippetDynamic[0]);
+	templateDynamic.source = &sourceDynamic;
+	templateDynamic.maxSrcLen = sizeof(sourceDynamic);
+	
 }
 
 void _CYCLIC ProgramCyclic(void)
 {
+	if(cmd.legacyCompile) {
+		cmd.legacyCompile = 0;
+		compileStatusLegacy = ChopCompile(&templateLegacy, sourceLegacy);	
+	}
+	if(cmd.legacyRender) {
+		cmd.legacyRender = 0;
+		renderStatusLegacy = ChopRender(destLegacy, &templateLegacy, sizeof(destLegacy), &renderLenLegacy);
+	}
+	
+	if(cmd.staticCompile) {
+		cmd.staticCompile = 0;
+		compileStatusStatic = ChopCompile(&templateStatic, sourceStatic);	
+	}
+	if(cmd.staticRender) {
+		cmd.staticRender = 0;
+		renderStatusStatic = ChopRender(destStatic, &templateStatic, sizeof(destStatic), &renderLenLegacy);
+	}
+	
+	if(cmd.dynamicCompile) {
+		cmd.dynamicCompile = 0;
+		compileStatusDynamic = ChopCompileGeneric(&templateDynamic, sourceDynamic);	
+	}
+	if(cmd.dynamicRender) {
+		cmd.dynamicRender = 0;
+		renderStatusDynamic = ChopRender(destDynamic, &templateDynamic, sizeof(destDynamic), &renderLenDynamic);
+	}
 
 }
 
--- example/AsProject/Logical/Programs/Default/Types.typ
+++ example/AsProject/Logical/Programs/Default/Types.typ
@@ -1,4 +1,11 @@
 
 TYPE
-
+	Cmd_typ : 	STRUCT 
+		legacyRender : BOOL;
+		staticRender : BOOL;
+		dynamicRender : BOOL;
+		legacyCompile : BOOL;
+		staticCompile : BOOL;
+		dynamicCompile : BOOL;
+	END_STRUCT;
 END_TYPE
